64ab6f6aedf41e9faa8d11b3e7b9c165b509eade
Søren Spangsberg Jørgensen <smsj@easv.dk>
2019-11-26 14:49:10 +0100

    First test route in api

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,7 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +9,25 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+//initialize firebase
+admin.initializeApp(functions.config().firebase);
+
+//initialize Express servers
+const app = express();
+const main = express();
+
+
+main.use('/api/v1', app); //add path used for receiving the request
+main.use(bodyParser.json()); //select JSON as the main parser for processing the request body
+
+
+//export function
+export const webApi = functions.https.onRequest(main);
+
+app.get('/warm', (req, res) => {
+    res.send('This the warm response and modified :)');
+})
+
+
+
